09082d2529f93b7da08f1d90951313f62a194c97
Fighter28 <zhengzhinan28@gmail.com>
2022-01-22 16:45:10 +0800

    Monet.tsx 整个UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,7 +5,11 @@ import styled from 'styled-components';
 const TagsSection = styled.section`
   background: #FFFFFF;
   padding: 12px 16px;
-
+  flex-grow: 1;
+  display: flex;  
+  flex-direction: column;
+  justify-content:flex-end;
+  align-items: flex-start;
   > ol {
     margin: 0 -12px;
 
@@ -72,16 +76,88 @@ const CateGorySection = styled.section`
         position: absolute;
         bottom: 0;
         width: 100%;
-        left:0;
+        left: 0;
       }
     }
   }
 `;
-const NumberPadSection = styled.section``;
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  > .output {
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+
+  }
+
+  > .pad {
+    > button {
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+
+      &.zero {
+        width: 50%;
+      }
+
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: #e0e0e0;
+      }
+
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: #d3d3d3;
+      }
+
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: #c1c1c1;
+      }
+
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: #b8b8b8;
+      }
+
+      &:nth-child(12) {
+        background: #9a9a9a;
+      }
+
+      &:nth-child(14) {
+        background: #a9a9a9;
+      }
+    }
+  }
+`;
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`;
 
 function Money() {
     return (
-        <Layout>
+        <MyLayout>
             <TagsSection>
                 <ol>
                     <li>衣</li>
@@ -97,15 +173,15 @@ function Money() {
                     <input type="text" placeholder="在这里添加备注"/>
                 </label>
             </NotesSection>
-            <CateGorySection >
+            <CateGorySection>
                 <ol>
-                    <li className='selected'>支出</li>
+                    <li className="selected">支出</li>
                     <li>收入</li>
                 </ol>
             </CateGorySection>
             <NumberPadSection>
-                <div>100</div>
-                <div>
+                <div className="output">100</div>
+                <div className="pad clearfix">
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
@@ -117,12 +193,13 @@ function Money() {
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button>OK</button>
-                    <button>0</button>
-                    <button>.</button>
+                    <button className="ok">OK</button>
+                    <button className="zero">0</button>
+                    <button className="dot">.</button>
                 </div>
             </NumberPadSection>
-        </Layout>);
+        </MyLayout>
+    )
 }
 
 export default Money;
