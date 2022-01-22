2dcb6211436d4a237c181dc4f5edfeac41603160
Fighter28 <zhengzhinan28@gmail.com>
2022-01-22 14:49:05 +0800

    完成tagList样式

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,27 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/Nomatch';
+import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/tags" element={<Tags/>}/>
-                <Route path="/money" element={<Money/>}/>
-                <Route path="/statistics" element={<Statistics/>}/>
-                <Route path="/" element={<Navigate replace to="/money"/>}/>
-                <Route path="*" element={<NoMatch/>}/>
-            </Routes>
-        </Router>
+        <AppWrapper>
+            <Router>
+                <Routes>
+                    <Route path="/tags" element={<Tags/>}/>
+                    <Route path="/money" element={<Money/>}/>
+                    <Route path="/statistics" element={<Statistics/>}/>
+                    <Route path="/" element={<Navigate replace to="/money"/>}/>
+                    <Route path="*" element={<NoMatch/>}/>
+                </Routes>
+            </Router>
+        </AppWrapper>
     );
 }
+
 export default App;
