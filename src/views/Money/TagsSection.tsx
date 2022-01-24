adceba7d023375a335ccef96e02e95fea899bbd5
Fighter28 <zhengzhinan28@gmail.com>
2022-01-24 14:16:27 +0800

    完成标签的选择和新增

---
+++ src/views/Money/TagsSection.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import React, {useState} from 'react';
+
+const Wrapper = styled.section`
+  background: #FFFFFF;
+  padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+
+  > ol {
+    margin: 0 -12px;
+
+    > li {
+      background-color: #D9D9D9;
+      display: inline-block;
+      border-radius: 18px;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+
+      &.selected {
+        background: burlywood;
+      }
+    }
+  }
+
+  > button {
+    background: none;
+    border: none;
+    border-bottom: 1px solid #333;
+    padding: 2px 4px;
+    color: #666;
+    margin-top: 10px;
+  }
+`;
+const TagsSection: React.FC = () => {
+    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+    const [selectedTag, setSelectedTag] = useState<string[]>([]);
+    const onToggleTag = (tag: string) => {
+        const index = selectedTag.indexOf(tag);
+        if (index >= 0) {
+            setSelectedTag(selectedTag.filter(t => t !== tag));
+        } else {
+            setSelectedTag([...selectedTag, tag]);
+        }
+    };
+    const onAddTag = () => {
+        const tagName = window.prompt('新标签的名称为');
+        if (tagName !== null) {
+            setTags([...tags, tagName]);
+        }
+    };
+    const getClass = (tag: string) => selectedTag.indexOf(tag) >= 0 ? 'selected' : '';
+    return (
+        <Wrapper>
+            <ol>
+                {tags.map(tag =>
+                    <li onClick={() => onToggleTag(tag)} className={getClass(tag)}
+                        key={tag}>{tag}</li>
+                )}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
+    );
+};
+
+export {TagsSection};
