c29ce94cbf5037e88d22a33f3534fa0fbb485e9a
Fighter28 <zhengzhinan28@gmail.com>
2022-02-04 13:32:41 +0800

    创建单独的Input组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.section`
+  display: flex;
+  align-items: center;
+
+  > span {
+    margin-right: 16px;
+    white-space: nowrap;
+  }
+
+  > input {
+    display: block;
+    width: 100%;
+    height: 72px;
+    border: none;
+    background: none;
+  }
+`;
+type Props = {
+    label: string,
+} & React.InputHTMLAttributes<HTMLInputElement>
+const Input: React.FC<Props> = (props) => {
+    const {label, children, ...rest} = props;
+    return (
+        <Label>
+            <span>{props.label}</span>
+            <input {...rest}/>
+        </Label>
+    );
+};
+export {Input};
